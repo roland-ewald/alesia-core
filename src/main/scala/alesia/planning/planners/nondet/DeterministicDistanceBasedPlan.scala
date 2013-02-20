de9c011cb0a5946084ed5cc7d763e48e67270013
Roland Ewald <roland.ewald@uni-rostock.de>
2013-02-20 17:25:05 +0100

    Prototype of plan that is based on state sets ordered by their distance to a goal state (in terms of actions).

--- src/main/scala/alesia/planning/planners/nondet/DeterministicDistanceBasedPlan.scala
+++ src/main/scala/alesia/planning/planners/nondet/DeterministicDistanceBasedPlan.scala
@@ -5,18 +5,33 @@ import alesia.planning.context.Context
 import alesia.planning.PlanningProblem
 
 /**
- * 
- * 
+ * Plan that is based on a list of state sets, ordered by their distance to the goal states
+ * (the set of goals is the first element of the array).
+ *
  * @author Roland Ewald
  */
-class DeterministicDistanceBasedPlan(val p:PlanningProblem, val distances: Array[Int]) extends Plan {
+class DeterministicDistanceBasedPlan(val problem: PlanningProblem, val distances: Array[Int]) extends Plan {
+  import problem.table._
 
-  override def decide(c: Context) = Seq()
-  
-  override def decide(c: Int) = {
-    
-  Seq()
+  override def decide(c: Context) = throw new UnsupportedOperationException
+
+  /** Check how far away from the goal the state is */
+  override def decide(state: Int) = {
+
+    val distanceIndex = distances.indexWhere(isContained(state, _))
+
+    if (distanceIndex <= 0)
+      Seq()
+    else
+      suitableActions(state, distanceIndex)
   }
-  
-  
+
+  /** Find actions that move the state towards the goal. */
+  def suitableActions(state: Int, distIndex: Int) = {
+    problem.actions.zipWithIndex.filter { a =>
+      isContained(state, a._1.weakPreImage(distances(distIndex - 1)))
+      //      isContained(preImg, distances(distIndex)) && !isContained(preImg, distances(distIndex - 1))
+    } map (_._2)
+  }
+
 }
