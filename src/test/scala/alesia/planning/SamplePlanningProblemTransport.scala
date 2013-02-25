617c98a6da05cfd729739bdaa8a94c651f047af9
Roland Ewald <roland.ewald@uni-rostock.de>
2013-02-25 18:23:22 +0100

    fixe encoding of actions...

--- src/test/scala/alesia/planning/SamplePlanningProblemTransport.scala
+++ src/test/scala/alesia/planning/SamplePlanningProblemTransport.scala
@@ -25,6 +25,16 @@ class SamplePlanningProblemTransport extends PlanningProblem {
   val posAirStation = v("pos=air-station")
   val posLuton = v("pos=Luton")
 
+  //  val positions = Array[Int](posTrainStation, posVictoriaStation, posGatwick, posCityCenter, posTruckStation, posAirStation, posLuton)
+  //
+  //  def noneExceptOne(fs: IndexedSeq[Int], positive: Int): Int = fs.zipWithIndex.foldLeft(1)((acc, f) => {
+  //    if (f._2 == positive)
+  //      table.and(acc, f._1)
+  //    else table.and(acc, table.not(f._1))
+  //  })
+  //
+  //  val invariant = (0 until positions.length).toList.map(noneExceptOne(positions, _)).foldLeft(0)(table.or)
+
   //Initial and goal state
   val initialState = (posTrainStation or posAirStation or (posTruckStation and fuel))
   val goalState = posGatwick
@@ -32,15 +42,15 @@ class SamplePlanningProblemTransport extends PlanningProblem {
   //Actions
   val driveTrain = action("drive-train",
     posTrainStation or (posVictoriaStation and lightIsGreen),
-    Effect(TrueVariable, add = List(posTrainStation), del = List(posVictoriaStation, posGatwick)),
+    Effect(posTrainStation, add = List(posVictoriaStation), del = List(posTrainStation, posGatwick)),
     Effect(posVictoriaStation and lightIsGreen, add = List(posGatwick), del = List(posVictoriaStation, posTrainStation)))
 
   val waitAtLight = action("wait-at-light", posVictoriaStation)
 
   val driveTruck = action("drive-truck",
     (posTruckStation and fuel) or (posCityCenter and fuel and !trafficJam),
-    Effect(TrueVariable, add = List(posTruckStation), del = List(posCityCenter, posGatwick)),
-    Effect(TrueVariable, add = List(posCityCenter), del = List(posGatwick, posTruckStation)),
+    Effect(posTruckStation, add = List(posCityCenter), del = List(posTruckStation, posGatwick)),
+    Effect(posCityCenter, add = List(posGatwick), del = List(posCityCenter, posTruckStation)),
     Effect(fuel, del = List(fuel), nondeterministic = true))
 
   val driveTruckBack = action("drive-truck-back",
