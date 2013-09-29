d3c3c18fac56e7142486f9e56c169a5bc6c94d44
Roland Ewald <roland.ewald@uni-rostock.de>
2013-09-29 16:05:54 +0200

    ALESIA-18: simplifying policy plan, determination of specific action is done by plan executor.

--- src/main/scala/alesia/planning/planners/nondet/PolicyPlan.scala
+++ src/main/scala/alesia/planning/planners/nondet/PolicyPlan.scala
@@ -12,26 +12,11 @@ import sessl.util.Logging
  */
 case class PolicyPlan(val policy: NonDeterministicPolicy) extends Plan with Logging {
   require(policy.stateActionTable.nonEmpty)
-  
+
   logger.info(s"Plan:\n\n${symbolicRepresentation}")
 
-  /**
-   * Decides upon an action, chooses the first one of which the preconditions are fulfilled.
-   * TODO: This strategy needs to be refined, otherwise one may end up within an infinite loop
-   */
-  override def decide(state: Int): Iterable[Int] = {
-    @tailrec
-    def decideFor(currentState: Int, stateActionPair: (Int, Int), stateActionPairs: Iterator[(Int, Int)]): Int = {
-      if (policy.problem.table.isContained(currentState, stateActionPair._1))
-        stateActionPair._2
-      else if (stateActionPairs.isEmpty)
-        throw new IllegalStateException(s"No match for state ${state}")
-      else
-        decideFor(currentState, stateActionPairs.next, stateActionPairs)
-    }
-    val stateActionPairs = policy.stateActionTable.iterator
-    List(decideFor(state, stateActionPairs.next, stateActionPairs))
-  }
+  override def decide(state: Int): Iterable[Int] =
+    policy.stateActionTable.filter(x => policy.problem.table.isContained(state, x._1)).map(_._2)
 
   lazy val symbolicRepresentation = policy.symbolicRepresentation
 }
